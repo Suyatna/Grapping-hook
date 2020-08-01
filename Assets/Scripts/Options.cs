0dfcec251a954981f8c267494e01a42efb27d6ec
Suyatna <zuyatna@gmail.com>
2020-08-01 21:31:31 +0700

    fixing options/gameplay

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Dropdown resolusi;
+    private Resolution[] _resolutions;
+
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res = _resolutions[resolutionIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+    }
+
+    private void Start()
+    {
+        _resolutions = Screen.resolutions;
+        resolusi.ClearOptions();
+        
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < _resolutions.Length; i++)
+        {
+            string option = _resolutions[i].width + "x" + _resolutions[i].height;
+            
+            options.Add(option);
+            if (_resolutions[i].width == Screen.currentResolution.width && _resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        
+        resolusi.AddOptions(options);
+        resolusi.value = currentResolutionIndex;
+        resolusi.RefreshShownValue();
+    }
+}
