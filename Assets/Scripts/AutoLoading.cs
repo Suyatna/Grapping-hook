904a3a1f55fdca8ab13d47e897add38fa0366bbe
Suyatna <zuyatna@gmail.com>
2020-08-03 13:04:02 +0700

    adding scene loading

---
+++ Assets/Scripts/AutoLoading.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AutoLoading : MonoBehaviour
+{
+    public GameObject loadingScreen;
+
+    public Slider slider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Manager.isLoadScene = false;
+        
+        StartCoroutine(LoadAsynchronously(2));
+        Time.timeScale = 1;
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+        
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+}
